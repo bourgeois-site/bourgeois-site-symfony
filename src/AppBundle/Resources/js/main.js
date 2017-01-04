16cf7b81a6a9b80919ae56871e577f737bdbc8c0
Akkuzin Maxim <akkmaxon2307@gmail.com>
2017-01-04 14:24:13 +0300

    Contacts are good

--- src/AppBundle/Resources/js/main.js
+++ src/AppBundle/Resources/js/main.js
@@ -1,17 +1,12 @@
-var setCustomerHeight = function() {
-  var element = $('main #customers .customer_logo');
-  element.css('height', element.css('width'));
-}
-
 var setFullScreenHeight = function(element) {
   var windowHeight = $(window).height();
   $(element).css('height', String(windowHeight) + "px");
 }
 
-var setServicesHeight = function() {
-  var headingHeight = $('#services .heading').height() + 100 + 40; // paddings
+var setWorksHeight = function() {
+  var headingHeight = $('#works .heading').height() + 80 + 30 + 50; // paddings + header
   var imageHeight = $(window).height() - headingHeight;
-  $('#services .item img').css('height', String(imageHeight) + "px");
+  $('#works .item img').css('height', String(imageHeight) + "px");
 }
 
 var cutServiceDescriptions = function() {
@@ -19,6 +14,18 @@ var cutServiceDescriptions = function() {
     var text = $(this).text();
     $(this).text(text.substring(0, 300) + "...");
   })
+}
+
+var setServicesHeight = function() {
+  var width = $(window).width();
+  var headingHeight = $('#services .heading').height() + 80 + 30 + 50; // paddings + header
+  var workHeight = 200; // default
+  if (width > 767 && width < 992) {
+    workHeight = ($(window).height() - headingHeight) / 3;
+  } else if (width > 991) {
+    workHeight = ($(window).height() - headingHeight) / 2;
+  }
+  $('#services .service').css('height', String(workHeight) + 'px');
 }
 
 $(document).ready(function() {
@@ -32,6 +39,26 @@ $(document).ready(function() {
 
   setFullScreenHeight('#jumbotron');
   setServicesHeight();
-  setCustomerHeight();
   cutServiceDescriptions();
+  setWorksHeight();
+
+  $('#services .service_description').slideUp(300);
+
+  $('#services .service').mouseenter(function() {
+    var caption = $(this).find('.service_caption');
+    caption.css('color', '#000');
+    caption.css('text-shadow', 'none');
+    caption.children('.service_description').slideDown(300);
+    var img = $(this).find('img');
+    img.css('opacity', '0.1');
+  });
+
+  $('#services .service').mouseleave(function() {
+    var caption = $(this).find('.service_caption');
+    caption.css('color', '#fff');
+    caption.css('text-shadow', '0 1px 1px #000');
+    caption.children('.service_description').slideUp(300);
+    var img = $(this).find('img');
+    img.css('opacity', '1.0');
+  });
 });
