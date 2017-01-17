2f539028a23eb9d499b6cc02e30d840f34124d93
Akkuzin Maxim <akkmaxon2307@gmail.com>
2017-01-17 14:50:17 +0300

    Pagination in admin/requests

--- src/AppBundle/Controller/AdminRequestsController.php
+++ src/AppBundle/Controller/AdminRequestsController.php
@@ -7,28 +7,68 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class AdminRequestsController extends Controller
 {
     /**
-     * @Route("/админ/заявки/новые", name="admin_new_requests")
+     * @Route("/админ/заявки/новые/{page}", name="admin_new_requests", defaults={"page": 1})
      */
-    public function indexNewAction()
+    public function indexNewAction($page)
     {
-        $requests = $this->getDoctrine()->
-            getRepository('AppBundle:Request')->
-            findByIsArchived(false, array('createdAt' => 'desc'));
+        $limit = 15;
+        $offset = $limit * ($page - 1);
 
-        return $this->render('admin/requests/index.html.twig',
-            ['type' => 'Новые', 'requests' => $requests]);
+        $repo = $this->getDoctrine()->getRepository('AppBundle:Request');
+
+        $allRequestsCount = sizeof($repo->findByIsArchived(false));
+
+        $pagesCount = ceil($allRequestsCount / $limit);
+
+        $query = $repo->createQueryBuilder('r')->
+            where('r.isArchived = false')->
+            orderBy('r.createdAt', 'desc')->
+            setFirstResult($offset)->
+            setMaxResults($limit)->
+            getQuery();
+
+        $requests = $query->getResult();
+
+        return $this->render('admin/requests/index.html.twig', [
+            'type' => 'Новые',
+            'page' => $page,
+            'pagesCount' => $pagesCount,
+            'path' => 'admin_new_requests',
+            'allRequestsCount' => $allRequestsCount,
+            'requests' => $requests
+        ]);
     }
 
     /**
-     * @Route("/админ/заявки/архив", name="admin_archived_requests")
+     * @Route("/админ/заявки/архив/{page}", name="admin_archived_requests", defaults={"page": 1})
      */
-    public function indexArchivedAction()
+    public function indexArchivedAction($page)
     {
-        $requests = $this->getDoctrine()->
-            getRepository('AppBundle:Request')->
-            findByIsArchived(true, array('createdAt' => 'desc'));
+        $limit = 15;
+        $offset = $limit * ($page - 1);
 
-        return $this->render('admin/requests/index.html.twig',
-            ['type' => 'Обработанные', 'requests' => $requests]);
+        $repo = $this->getDoctrine()->getRepository('AppBundle:Request');
+
+        $allRequestsCount = sizeof($repo->findByIsArchived(true));
+
+        $pagesCount = ceil($allRequestsCount / $limit);
+
+        $query = $repo->createQueryBuilder('r')->
+            where('r.isArchived = true')->
+            orderBy('r.createdAt', 'desc')->
+            setFirstResult($offset)->
+            setMaxResults($limit)->
+            getQuery();
+
+        $requests = $query->getResult();
+
+        return $this->render('admin/requests/index.html.twig', [
+            'type' => 'Обработанные',
+            'page' => $page,
+            'pagesCount' => $pagesCount,
+            'path' => 'admin_archived_requests',
+            'allRequestsCount' => $allRequestsCount,
+            'requests' => $requests
+        ]);
     }
 }
