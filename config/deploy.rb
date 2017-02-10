90087c176e97cf110f2b0e68fa78cd17c81b7e8d
Akkuzin Maxim <akkmaxon2307@gmail.com>
2017-02-10 16:10:05 +0300

    Changed dir for storing files and photos

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :application, "bourgeois"
 set :repo_url, "git@github.com:bourgeois-site/bourgeois-site-symfony.git"
 
 set :linked_files, %w(app/config/parameters.yml)
-set :linked_dirs, %w(var vendor web/images)
+set :linked_dirs, %w(var vendor web/files)
 
 set :keep_releases, 5
 
